160f0ddda8c3f6f69bf801f5df15223cc00fa558
LeoCristobal <leocristobal108@gmail.com>
2026-01-23 18:29:47 +0800

    Basic Project 9: Form Validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import Accordion from "./Components/Accordion";
-import { accordionData } from "./utils/accordionList.js";
+import FormValidation from "./Components/FormValidation";
+
 const App = () => {
     return (
-        <div>
-            <div className="accordion">
-                {accordionData.map(({ title, content }) => (
-                    <Accordion
-                        title={title}
-                        content={content}
-                        key={Math.random() * 2000}
-                    />
-                ))}
+        <>
+            <div className="container">
+                <FormValidation />
             </div>
-        </div>
+        </>
     );
 };
 
